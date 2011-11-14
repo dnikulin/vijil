3eb11ee2b3fb8ed00ce4d5635e82514f0172fc88
Dmitri Nikulin <dnikulin@gmail.com>
2011-11-14 19:20:38 +1100

    Make XmlTools.readNode() fast and paranoid

--- src/main/scala/com/dnikulin/vijil/tools/XmlTools.scala
+++ src/main/scala/com/dnikulin/vijil/tools/XmlTools.scala
@@ -22,7 +22,12 @@ package com.dnikulin.vijil.tools
 
 import scala.xml._
 
+import javax.xml._
+import javax.xml.parsers._
+
 import java.io.ByteArrayInputStream
+
+import net.liftweb.util.Helpers.tryo
 
 object XmlTools {
   def attribute(node: Node, key: String): Option[String] =
@@ -37,6 +42,23 @@ object XmlTools {
   def childStrings(node: Node, name: String): Array[String] =
     (node \ name).toArray.map(_.text)
 
-  def readNode(bytes: Array[Byte]): Option[Node] =
-    TryOrNone(Some(XML.load(new ByteArrayInputStream(bytes))))
+  def readNode(bytes: Array[Byte]): Option[Node] = tryo {
+    // Create paranoid XML parser factory.
+    val prefix = "http://xml.org/sax/features/"
+    val factory = SAXParserFactory.newInstance()
+    factory.setNamespaceAware(false)
+    factory.setValidating(false)
+    factory.setXIncludeAware(false)
+    factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true)
+    factory.setFeature(prefix + "external-general-entities", false)
+    factory.setFeature(prefix + "external-parameter-entities", false)
+    factory.setFeature(prefix + "validation", false)
+
+    // Create input stream.
+    val stream = new ByteArrayInputStream(bytes)
+
+    // Create and deploy paranoid XML parser.
+    val parser = factory.newSAXParser()
+    XML.withSAXParser(parser).load(stream)
+  }
 }
