1c73c382cf92837020b09ed549123e2bf55d5235
Dmitri Nikulin <dnikulin@gmail.com>
2011-12-08 12:41:30 +1100

    Draft GZipStack and JsonStack convenience builders

---
+++ src/main/scala/com/dnikulin/vijil/store/JsonStack.scala
@@ -0,0 +1,31 @@
+// Copyright (C) 2011  Dmitri Nikulin
+//
+// This file is part of Vijil.
+//
+// Vijil is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Vijil is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Vijil.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Repository:     https://github.com/dnikulin/vijil
+// Email:          dnikulin+vijil@gmail.com
+
+package com.dnikulin.vijil.store
+
+import com.dnikulin.vijil.traits.ToJson
+
+object JsonStack {
+  def apply[T <: ToJson](byteStore: ObjectStore[Array[Byte]], fromJson: (Array[Byte] => Option[T])): ObjectStore[T] = {
+    val gzipStore = new ByteFilterStore(byteStore, GZipByteFilter)
+    val jsonStore = new JsonStore[T](gzipStore, fromJson)
+    return new CacheStore(jsonStore)
+  }
+}
