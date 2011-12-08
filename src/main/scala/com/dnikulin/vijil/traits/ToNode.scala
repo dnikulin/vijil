a11956f789af46140893d22fbc1de1bae8ba503d
Dmitri Nikulin <dnikulin@gmail.com>
2011-12-08 15:28:56 +1100

    Add simple ToNode and FromNode traits

---
+++ src/main/scala/com/dnikulin/vijil/traits/ToNode.scala
@@ -0,0 +1,34 @@
+// Copyright (C) 2011  Dmitri Nikulin
+//
+// This file is part of Vijil.
+//
+// Vijil is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Vijil is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Vijil.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Repository:     https://github.com/dnikulin/vijil
+// Email:          dnikulin+vijil@gmail.com
+
+package com.dnikulin.vijil.traits
+
+import scala.xml.Node
+
+trait ToNode {
+  def toNode: Node
+}
+
+trait FromNode[+T] {
+  def fromNode(node: Node): Option[T]
+
+  def fromNodes(nodes: Seq[Node]): Seq[T] =
+    nodes.flatMap(fromNode)
+}
